d5082f089d9952585bf2d0cf322bf017faca60d3
pedro henrique silva rodrigues <ph8464803@gmail.com>
2024-01-10 16:37:52 -0300

    contador de tentativas

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ alert("Boas vindas ao jogo mundo secreto");
 let numeroSecreto = 5;
 
 let chute;
+let tenetativas = 1;
 
 while (chute != numeroSecreto) {
     chute = prompt("Escolha um numero de 1 a 30");
     if (numeroSecreto == chute) {
-        alert("acertou")
+        alert("acertou o numero é " + numeroSecreto + " e o numeros de tentais foi " + tenetativas)
     } else {
         if (numeroSecreto > chute) {
             alert("O numero secreto e menor que " + chute);
         } else {
             alert("O numero secreto e menor que " + chute);
         }
+        tenetativas++;
 
     }
 }
